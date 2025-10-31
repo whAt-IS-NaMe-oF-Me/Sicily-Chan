5a3275140e818a5515160949ae76089ac0a4d4a0
欧阳诚 <1684155413@qq.com>
2025-10-31 15:24:04 +0800

    Update script.min.user.js

--- script.min.user.js
+++ script.min.user.js
@@ -4,7 +4,7 @@
 // @version      2025-10-30
 // @description  try to take over the world!
 // @author       You
-// @match        https://oj.turingedu.cn/*
+// @match        https://*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=soj.ac
 // @grant        none
 // ==/UserScript==
@@ -95,6 +95,7 @@ $(function () {
   }).then(p)))();
 
 });
+
 
 
 
