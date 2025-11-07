b6da026f45ad33d8f1433aa8d7060c3adf937540
欧阳诚 <1684155413@qq.com>
2025-11-07 20:05:56 +0800

    Update Sicily Chan.min.user.js

--- Sicily Chan.min.user.js
+++ Sicily Chan.min.user.js
@@ -57,44 +57,47 @@ $(function () {
     document.body.appendChild(form);
     form.submit();
   });
+  
+  if (window.location.href != "https://soj.ac/")
+  {
+    $("body").append(`
+        <div id="sicily_chan">
+            <div id="talkbox">
+                <div id="boxtop"></div>
+                <div id="boxcnt">
+                    <div>自定义内容：</div>
+                    <div style="text-align: center; ">参考这样换行与缩进！</div>
+                </div>
+                <div id="boxbottom"></div>
+            </div>
+            <div id="sicilychan_body">
+                <div id="face" class="face1"></div>
+            </div>
+        </div>
+    `);
 
-  $("body").append(`
-      <div id="sicily_chan">
-          <div id="talkbox">
-              <div id="boxtop"></div>
-              <div id="boxcnt">
-                  <div>自定义内容：</div>
-                  <div style="text-align: center; ">参考这样换行与缩进！</div>
-              </div>
-              <div id="boxbottom"></div>
-          </div>
-          <div id="sicilychan_body">
-              <div id="face" class="face1"></div>
-          </div>
-      </div>
-  `);
+    $("#sicily_chan").draggable();
 
-  $("#sicily_chan").draggable();
-
-  var p;
-  (p = () => new Promise((resolve) => {
-      setTimeout(() => {
-          $("#face").removeClass("face1").addClass("face2");
-          resolve();
-      }, 2000 + Math.random() * 2000);
-  }).then(() => new Promise((resolve) => {
-      setTimeout(() => {
-          $("#face").removeClass("face2").addClass("face3");
-          resolve();
-      }, 100);
-  })).then(() => new Promise((resolve) => {
-      setTimeout(() => {
-          $("#face").removeClass("face3").addClass("face1");
-          resolve();
-      }, 100);
-  }).then(p)))();
-
+    var p;
+    (p = () => new Promise((resolve) => {
+        setTimeout(() => {
+            $("#face").removeClass("face1").addClass("face2");
+            resolve();
+        }, 2000 + Math.random() * 2000);
+    }).then(() => new Promise((resolve) => {
+        setTimeout(() => {
+            $("#face").removeClass("face2").addClass("face3");
+            resolve();
+        }, 100);
+    })).then(() => new Promise((resolve) => {
+        setTimeout(() => {
+            $("#face").removeClass("face3").addClass("face1");
+            resolve();
+        }, 100);
+    }).then(p)))();
+  }
 });
+
 
 
 
